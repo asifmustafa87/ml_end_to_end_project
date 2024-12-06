d9b2e7d1005d6452d61c37e511d3ba539c0b3887
Asif Mustafa <asifibnamustafa87@gmail.com>
2024-12-06 11:20:42 +0100

    model trainer

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -49,8 +49,7 @@ class DataTransformation:
 
                 steps=[
                 ("imputer",SimpleImputer(strategy="most_frequent")),
-                ("one_hot_encoder",OneHotEncoder()),
-                ("scaler",StandardScaler(with_mean=False))
+                ("one_hot_encoder",OneHotEncoder())
                 ]
 
             )
